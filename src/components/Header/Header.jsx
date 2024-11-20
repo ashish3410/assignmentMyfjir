1d5b19c98ab2a9e6734059e5fc18eda8ed768075
ashish <148787104+ashish3410@users.noreply.github.com>
2024-11-20 13:21:41 +0530

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,8 +3,8 @@ function Header() {
     return (
         <header className='hidden sm:flex justify-between items-center bg-gray-100 py-2'>
            <div className='flex gap-6 px-4'>
-           <a className='hover:underline' href="/">For you</a>
-           <a className='hover:underline' href="/for-business">For business</a>
+           <a className='hover:underline' href="/for-you">For you</a>
+           <a className='hover:underline' href="/">For business</a>
            </div>
            <div className=' flex text-[13px] gap-2 px-4 items-center'>
                 <img className='h-4 w-4' src="https://img.icons8.com/?size=24&id=bx6JPy0W0loj&format=png" alt="" />
